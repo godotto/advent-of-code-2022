c04d2281b6479f211d061b09b4fb21d044a464ae
godotto <m.marcinkiewicz@protonmail.com>
2022-12-02 00:09:04 +0100

    Day 1 puzzles solution

---
+++ Day 1/Program.cs
@@ -0,0 +1,41 @@
+﻿void MaxCalories()
+{
+    var caloriesSum = 0;
+    var maxCalories = 0;
+
+    foreach (var line in File.ReadLines(args[0]))
+    {
+        if (line.Length == 0)
+        {
+            maxCalories = caloriesSum > maxCalories ? caloriesSum : maxCalories;
+            caloriesSum = 0;
+        }
+        else
+            caloriesSum += Convert.ToInt32(line);
+    }
+
+    Console.WriteLine($"Max number of calories is {maxCalories}");
+}
+
+void MaxCaloriesOutOfThree()
+{
+    var caloriesSum = 0;
+    int[] maxCalories = {0, 0, 0};
+
+    foreach (var line in File.ReadLines(args[0]))
+    {
+        if (line.Length == 0)
+        {
+            maxCalories[0] = caloriesSum > maxCalories[0] ? caloriesSum : maxCalories[0];
+            Array.Sort(maxCalories);
+            caloriesSum = 0;
+        }
+        else
+            caloriesSum += Convert.ToInt32(line);
+    }
+
+    Console.WriteLine($"Max number of calories from top three elves is {maxCalories[0] + maxCalories[1] + maxCalories[2]}");
+}
+
+MaxCalories();
+MaxCaloriesOutOfThree();
